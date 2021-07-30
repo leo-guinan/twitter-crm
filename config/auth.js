8dde72beab025edb6d64e71db32f06800a13dc50
leo-guinan <leo@guinantech.com>
2021-07-30 17:01:13 -0400

    Inital commit

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'twitterAuth' : {
+        'consumerKey'       : process.env.CONSUMER_KEY,
+        'consumerSecret'    : process.env.CONSUMER_SECRET,
+        'callbackURL'       : process.env.CALLBACK_URL
+    }
+
+};
